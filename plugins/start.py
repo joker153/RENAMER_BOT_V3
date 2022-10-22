9804d3239ee57fb097475e2fe13888cc6338fb92
Vinesh <92975459+Vineshvinu@users.noreply.github.com>
2022-10-22 13:05:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -35,8 +35,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filename to rename it__
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/THM_OTT") ], 
+	[InlineKeyboardButton("Owner😎", url="https://t.me/Vinesh_vinu") ]  ]))
 
 
 
