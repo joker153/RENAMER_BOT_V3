cf7f20c0fae61402234d9ee8ab31bdff4c29d917
joker153 <77005994+joker153@users.noreply.github.com>
2024-07-04 16:06:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ API_ID = int(os.environ.get("API_ID", "6119632"))
 
 API_HASH = os.environ.get("API_HASH", "5b51582f938e93c8eab32612d5045737")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "1BVtsOJ4Bu4p5EEnU5dLWCWl-nE9yste6vR_44tFWlySK5i_IwC9vG_Bue7bELVb16EEwnSTZMKlNm7MsaPtt2YE-5_rD8cZAX6Up9-gc8CGzHg0-Bh5-3H0PcfWPNYoSMFXpR-InAHBbYcU8u31pfunIXhhtRYaSg3f0mgabLw9uKFnKXT7WOv1NKxwl0iO83xMntzMxvs0eSyuG_YP5A0cHT1_DNTAZfKjZ2kLN9ek-W3-6Tn0I9CVXQrPINMDiWrVXSGP_1nahadWiG8Nwpy7Z3-SK3Gn77BT0_XvnLqc0aNE0NrjPc6uAdvqwod_uVBIiSVqPEXdsWlagZ7Q_bqG097M27UQ=")
 
 ADMIN = int(os.environ.get("ADMIN", 1869495895))
 
@@ -12,6 +12,6 @@ log_channel = int(os.environ.get("LOG_CHANNEL", "-1002049486679"))
 
 CHANNEL = os.environ.get('CHANNEL',"")
 
-DB_NAME = os.environ.get("DB_NAME","")
+DB_NAME = os.environ.get("DB_NAME","renam")
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://amal:amal@cluster0.6qbjqaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
