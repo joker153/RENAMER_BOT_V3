72759b682d8676ddb5ee2c884ac4de559542216e
joker153 <77005994+joker153@users.noreply.github.com>
2024-07-04 15:27:56 +0530

    Update premiumdb.py

--- helper/premiumdb.py
+++ helper/premiumdb.py
@@ -1,8 +1,7 @@
 import pymongo 
 import os
 from helper.date import add_date
-DB_NAME = os.environ.get("DB_NAME","")
-DB_URL = os.environ.get("DB_URL","")
+from info.py import *
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["premium"]
