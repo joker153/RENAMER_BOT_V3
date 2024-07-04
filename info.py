4f0b7a9ffde93bc8538b1a8d27f37ed32634f84d
joker153 <77005994+joker153@users.noreply.github.com>
2024-07-04 15:57:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,14 +1,14 @@
 TOKEN = os.environ.get("TOKEN", "6426072097:AAFpvNEzW65qVT6XwTxzafDLJKFk-Xtbun0")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "6119632"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "5b51582f938e93c8eab32612d5045737")
 
 STRING = os.environ.get("STRING", "")
 
-ADMIN = int(os.environ.get("ADMIN", 923943045))
+ADMIN = int(os.environ.get("ADMIN", 1869495895))
 
-log_channel = int(os.environ.get("LOG_CHANNEL", ""))
+log_channel = int(os.environ.get("LOG_CHANNEL", "-1002049486679"))
 
 CHANNEL = os.environ.get('CHANNEL',"")
 
